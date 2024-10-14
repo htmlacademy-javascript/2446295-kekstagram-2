fd5d84a1f270dd92eb8bee3b34a275eadfc2f0b1
Владимир Симонов <2172395@gmail.com>
2024-10-14 18:39:20 +0300

    added imageEdit.js

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@ import {getPhotos} from './data.js';
 import { createThumbnails } from './thumbnails.js';
 import './form.js';
 import './validateForm.js';
+import './imageEdit.js';
 
 const photos = getPhotos();
 createThumbnails(photos);
